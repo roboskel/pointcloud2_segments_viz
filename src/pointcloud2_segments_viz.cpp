fe34100287c6e32f8e5a473062b80f3fdd9ba992
Katerina Oikonomou <cathrine.m.oik@gmail.com>
2018-06-12 14:16:11 +0300

    Code imported from koikonomou/euclidean_clustering

---
+++ src/pointcloud2_segments_viz.cpp
@@ -0,0 +1,84 @@
+#include <ros/ros.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/common/colors.h>
+#include <pcl_ros/point_cloud.h>
+#include <pointcloud_msgs/PointCloud2_Segments.h>
+#include <sensor_msgs/PointCloud.h>
+#include <pcl/impl/point_types.hpp>
+#include <sensor_msgs/PointCloud2.h>
+#include <sensor_msgs/ChannelFloat32.h>
+#include <pcl/common/projection_matrix.h>
+#include <sensor_msgs/point_cloud_conversion.h>
+
+
+ros::Publisher pub;
+std::string base_link_frame;
+
+
+void pc2s_callback (const pointcloud_msgs::PointCloud2_Segments& msg){
+
+    sensor_msgs::PointCloud2 accumulator;
+
+    sensor_msgs::PointCloud2 cluster_msgs;
+
+    for (size_t i=0; i < msg.clusters.size(); i++){
+
+        pcl::PCLPointCloud2 cloud2;
+        pcl_conversions::toPCL( msg.clusters[i] , cloud2);
+
+        pcl::PointCloud<pcl::PointXYZRGB> cloud;
+        pcl::fromPCLPointCloud2(cloud2, cloud);
+
+        if (msg.cluster_id.size() > 0 ){
+            for (int u=0; u < msg.cluster_id.size(); u++){
+                // ROS_WARN("%u", msg.cluster_id[u]);
+
+                for(size_t j=0; j < cloud.points.size(); j++){
+                    uint8_t r = 255 - 25 * u^8 *i;
+                    uint8_t g = 90 + 40 * u^8 *i;
+                    uint8_t b = 40; 
+                    int32_t rgb = (r << 16) | (g << 8) | b;
+                    cloud.points[j].rgb = *(float*)(&rgb);
+                }
+            }
+        }
+        else {
+            for(size_t j=0; j < cloud.points.size(); j++){
+                uint8_t r = 255 - 25 * i;
+                uint8_t g = 90 + 40 * i;
+                uint8_t b = 40;
+                int32_t rgb = (r << 16) | (g << 8) | b;
+                cloud.points[j].rgb = *(float*)(&rgb);
+            }
+        }
+        pcl::PCLPointCloud2 clouds;
+        pcl::toPCLPointCloud2(cloud, clouds);
+        pcl_conversions::fromPCL(clouds, cluster_msgs);
+        cluster_msgs.header.frame_id = base_link_frame;
+
+        sensor_msgs::PointCloud2 tmp = sensor_msgs::PointCloud2(accumulator);
+
+        pcl::concatenatePointCloud( cluster_msgs, tmp, accumulator);
+    }
+
+    pub.publish(accumulator);
+}
+
+int main (int argc, char** argv){
+    ros::init (argc, argv, "pointcloud2_segments_viz");
+    ros::NodeHandle n_;
+    pcl::console::setVerbosityLevel(pcl::console::L_ALWAYS);
+
+    std::string input_topic;
+    std::string out_topic;
+    n_.param("pointcloud2_segments_viz/input_topic",input_topic, std::string("/new_pcl"));
+    n_.param("pointcloud2_segments_viz/base_link_frame", base_link_frame, std::string("base_link"));
+    n_.param("pointcloud2_segments_viz/out_topic", out_topic, std::string("pointcloud2_segments_viz/pointcloud2"));
+
+    ros::Subscriber sub = n_.subscribe (input_topic, 1, pc2s_callback);
+
+    pub = n_.advertise<sensor_msgs::PointCloud2> (out_topic, 1);
+
+    ros::spin ();
+}
